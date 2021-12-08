6bca37127cf94632ebd57bc23d376d99fa7b4a46
vishvesh27-engg <vishveshwarrier@gmail.com>
2021-12-08 10:34:56 +0530

    chatpage done

---
+++ lib/chatpage.dart
@@ -0,0 +1,141 @@
+import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class chat_page extends StatefulWidget {
+  final docs;
+  const chat_page({Key? key, this.docs}) : super(key: key);
+
+  @override
+  _chat_pageState createState() => _chat_pageState();
+}
+
+class _chat_pageState extends State<chat_page> {
+  String groupChatId = '';
+  String? userID;
+
+  TextEditingController textEditingController = TextEditingController();
+
+  ScrollController scrollController = ScrollController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    getcommonid();
+    super.initState();
+  }
+
+  getcommonid() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    userID = sharedPreferences.getString('id');
+    String otherid = widget.docs['id'];
+    if (userID!.compareTo(otherid) > 0) {
+      groupChatId = '$userID - $otherid';
+    } else {
+      groupChatId = '$otherid - $userID';
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat page!'),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('messages')
+              .doc(groupChatId)
+              .collection(groupChatId)
+              .orderBy('timestamp', descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData && snapshot.data != null) {
+              return Column(
+                children: <Widget>[
+                  Expanded(
+                      child: ListView.builder(
+                    itemBuilder: (listContext, index) =>
+                        buildItem(snapshot.data!.docs[index]),
+                    itemCount: snapshot.data!.docs.length,
+                    controller: scrollController,
+                    reverse: true,
+                  )),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          controller: textEditingController,
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.send),
+                        onPressed: () => sendmsg(),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            } else {
+              return const Center(
+                  child: SizedBox(
+                height: 36,
+                width: 36,
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                ),
+              ));
+            }
+          }),
+    );
+  }
+
+  sendmsg() {
+    String msg = textEditingController.text.trim();
+    if (msg.isNotEmpty) {
+      var ref = FirebaseFirestore.instance
+          .collection('messages')
+          .doc(groupChatId)
+          .collection(groupChatId)
+          .doc(DateTime.now().millisecondsSinceEpoch.toString());
+
+      FirebaseFirestore.instance.runTransaction((transaction) async {
+        await transaction.set(ref, {
+          "senderId": userID,
+          "anotherUserId": widget.docs['id'],
+          "timestamp": DateTime.now().millisecondsSinceEpoch.toString(),
+          'content': msg,
+          "type": 'text',
+        });
+      });
+      scrollController.animateTo(0.0,
+          duration: Duration(milliseconds: 100), curve: Curves.bounceInOut);
+    } else {
+      print('Please enter some text to send');
+    }
+  }
+
+  buildItem(doc) {
+    return Padding(
+      padding: EdgeInsets.only(
+          top: 8.0,
+          left: ((doc['senderId'] == userID) ? 64 : 0),
+          right: ((doc['senderId'] == userID) ? 0 : 64)),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+            color: ((doc['senderId'] == userID)
+                ? Colors.grey
+                : Colors.greenAccent),
+            borderRadius: BorderRadius.circular(8.0)),
+        child: (doc['type'] == 'text')
+            ? Text('${doc['content']}')
+            : Image.network(doc['content']),
+      ),
+    );
+  }
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,10 @@
+import 'package:chat_app/chatpage.dart';
+import 'package:chat_app/login.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class homepage extends StatefulWidget {
   const homepage({Key? key}) : super(key: key);
@@ -8,8 +14,62 @@ class homepage extends StatefulWidget {
 }
 
 class _homepageState extends State<homepage> {
+  String? userid;
+  @override
+  void initState() {
+    // TODO: implement initState
+    getuserid();
+    super.initState();
+  }
+
+  getuserid() async {
+    SharedPreferences sharedpreferences = await SharedPreferences.getInstance();
+    userid = sharedpreferences.getString('id');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            ElevatedButton(
+                onPressed: () async {
+                  await GoogleSignIn().signOut();
+                  SharedPreferences sharedPrefs =
+                      await SharedPreferences.getInstance();
+                  sharedPrefs.setString('id', '');
+                  Navigator.of(context).pop();
+                },
+                child: Text('Logout'))
+          ],
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('users').snapshots(),
+            builder: (context, snapshot) => ListView.builder(
+                  itemBuilder: (listContext, index) =>
+                      buildItem(snapshot.data!.docs[index]),
+                  itemCount: snapshot.data!.docs.length,
+                )));
+  }
+
+  buildItem(doc) {
+    return (doc['id'] != userid)
+        ? GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => chat_page(docs: doc)));
+            },
+            child: Card(
+              color: Colors.lightBlue,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Center(
+                    child: Text(doc['name']),
+                  ),
+                ),
+              ),
+            ))
+        : Container();
   }
 }
